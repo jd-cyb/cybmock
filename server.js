442dc11995b997228e573bb0d900b63aa7751e0e
furic <furic@qq.com>
2018-10-16 16:59:07 +0800

    严格定义http请求

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use((req, res, next) => {
 
 // 添加json解析
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.use('/static', express.static(path.join(__dirname, './public')))
 app.set('views', path.join(__dirname, './views'))
@@ -35,7 +37,7 @@ app.set('view engine', 'html')
 /**
  * [@index 从 1 开始]
  */
-Handlebars.registerHelper("inc", function(value, options) {
+Handlebars.registerHelper("inc", function (value, options) {
   return parseInt(value) + 1;
 })
 
@@ -44,8 +46,7 @@ const cacheFile = path.join(process.cwd(), './.cache')
 let interface = [] //接口列表
 
 mockMiddleware().filter(item => {
-  app.all(item.route, item.handle)
-
+  app[item.method](item.route, item.handle)
   interface.push({
     method: item.method,
     route: item.route
@@ -57,14 +58,20 @@ const serverIp = internalIp.v4.sync() || 'localhost' //服务器IP
 let serverPort = 0
 
 app.get('/', (req, res) => {
-  res.render('index', { projectName: process.cwd().split(path.sep).pop(), interface: interface, serverIp: serverIp, serverPort: serverPort })
+  res.render('index', {
+    projectName: process.cwd().split(path.sep).pop(),
+    interface: interface,
+    serverIp: serverIp,
+    serverPort: serverPort
+  })
 })
 
-
-portscanner.findAPortNotInUse(3000, 3050, '127.0.0.1', function(error, port) {
+portscanner.findAPortNotInUse(3000, 3050, '127.0.0.1', function (error, port) {
   serverPort = port //服务器端口
 
-  fsExtra.writeJson(cacheFile, { port: port }, err => {
+  fsExtra.writeJson(cacheFile, {
+    port: port
+  }, err => {
     if (err) return console.error(err)
     app.listen(port, () => console.log(`Mock Server listening on port ${port}!`))
   })
